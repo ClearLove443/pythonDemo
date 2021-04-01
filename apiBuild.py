e7aea9d01e84b7e040c748f94d7a02f791b9b1fa
qiangqiang.a.zhang <qiangqiang.a.zhang@accenture.com>
2021-04-01 16:17:04 +0800

    mod

--- apiBuild.py
+++ apiBuild.py
@@ -8,13 +8,14 @@ filepath = 'C:\develop\workspace_vscode\\'
 def run_bat(i):  # 定义运行python文件函数
 
     cmd1 = 'cd ' + filepath + i
-    cmd2 = '.\gradlew build'
-    cmd = cmd1 + ' && ' + cmd2
+    cmd2 = 'git pull'
+    cmd3 = '.\gradlew build'
+    cmd = cmd1 + ' && ' + cmd2 + ' && ' + cmd3
     os.system(cmd)
 
 
 if __name__ == '__main__':  # 多线程运行python文件
+
     for i in reports:
         task = threading.Thread(target=run_bat, args=(i, ))
         task.start()
-
