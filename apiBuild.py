edea69d79e8e706334c166c9a3cafb0effb7cdcd
qiangqiang.a.zhang <qiangqiang.a.zhang@accenture.com>
2021-04-01 16:35:16 +0800

    mod

--- apiBuild.py
+++ apiBuild.py
@@ -9,8 +9,9 @@ def run_bat(i):  # 定义运行python文件函数
 
     cmd1 = 'cd ' + filepath + i
     cmd2 = 'git pull'
-    cmd3 = '.\gradlew build'
-    cmd = cmd1 + ' && ' + cmd2 + ' && ' + cmd3
+    cmd3 = 'echo pull ' + i + ' finished'
+    cmd4 = '.\gradlew build'
+    cmd = cmd1 + ' && ' + cmd2 + ' && ' + cmd3 + ' && ' + cmd4
     os.system(cmd)
 
 
